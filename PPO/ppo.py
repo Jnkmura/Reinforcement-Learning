5c9beb6a72260fc16c4386c3729d24c44f39302a
mk <jnkami@gmail.com>
2019-09-15 14:24:30 -0300

    fixing utils

--- PPO/ppo.py
+++ PPO/ppo.py
@@ -11,51 +11,7 @@ from keras.models import Model
 from keras.layers.merge import concatenate, Add
 from gym.spaces import Box, Discrete
 from utils.experience_replay import PPOBuffer
-from gym.core import Wrapper, ObservationWrapper
-from scipy.misc import imresize
-
-class Preprocess(ObservationWrapper):
-    def __init__(self, env):
-        ObservationWrapper.__init__(self,env)
-
-        self.img_size = (env.observation_space.shape[0],
-                         env.observation_space.shape[1],
-                         1)
-        self.observation_space = Box(0.0, 1.0, self.img_size
-                                     , dtype=np.float32)
-
-    def observation(self, img):        
-        
-        # resize and normalize img
-        img = imresize(img, self.img_size)
-        img = img.mean(-1, keepdims=True)
-        img = img.astype('float32') / 255.
-        
-        return img
-
-class FrameBuffer(Wrapper):
-    # stack 4 frames into one state
-    def __init__(self, env, n_frames=4):
-        super(FrameBuffer, self).__init__(env)
-        height, width, n_channels = env.observation_space.shape
-        obs_shape = [height, width, n_channels * n_frames]
-        self.observation_space = Box(0.0, 1.0, obs_shape, dtype=np.float32)
-        self.framebuffer = np.zeros(obs_shape, 'float32')
-        
-    def reset(self):
-        self.framebuffer = np.zeros_like(self.framebuffer)
-        self.update_buffer(self.env.reset())
-        return self.framebuffer
-    
-    def step(self, action):
-        new_img, reward, done, info = self.env.step(action)
-        self.update_buffer(new_img)
-        return self.framebuffer, reward, done, info
-    
-    def update_buffer(self, img):
-        offset = self.env.observation_space.shape[-1]
-        cropped_framebuffer = self.framebuffer[:,:,:-offset]
-        self.framebuffer = np.concatenate([img, cropped_framebuffer], axis = -1)
+from utils.img_utils import FrameBuffer, Preprocess
 
 def gaussian_likelihood(x, mu, log_std):
     pre_sum = -0.5 * (((x-mu)/(tf.exp(log_std)+1e-8))**2 + 2*log_std + np.log(2*np.pi))
@@ -254,8 +210,9 @@ class PPO:
 
     def create_env(self):
         env = gym.make(self.env_name)
-        env = Preprocess(env)
-        env = FrameBuffer(env, n_frames=4)
+        if len(env.observation_space.shape) > 2:
+            env = Preprocess(env)
+            env = FrameBuffer(env, n_frames=4)
         return env
 
 if __name__=='__main__':
---
+++ utils/img_utils.py
@@ -0,0 +1,51 @@
+
+from gym.core import Wrapper, ObservationWrapper
+from scipy.misc import imresize
+from gym.spaces import Box, Discrete
+import numpy as np
+
+class FrameBuffer(Wrapper):
+    # stack 4 frames into one state
+    def __init__(self, env, n_frames=4):
+        super(FrameBuffer, self).__init__(env)
+        height, width, n_channels = env.observation_space.shape
+        obs_shape = [height, width, n_channels * n_frames]
+        self.observation_space = Box(0.0, 1.0, obs_shape, dtype=np.float32)
+        self.framebuffer = np.zeros(obs_shape, 'float32')
+        
+    def reset(self):
+        self.framebuffer = np.zeros_like(self.framebuffer)
+        self.update_buffer(self.env.reset())
+        return self.framebuffer
+    
+    def step(self, action):
+        new_img, reward, done, info = self.env.step(action)
+        self.update_buffer(new_img)
+        return self.framebuffer, reward, done, info
+    
+    def update_buffer(self, img):
+        offset = self.env.observation_space.shape[-1]
+        cropped_framebuffer = self.framebuffer[:,:,:-offset]
+        self.framebuffer = np.concatenate([img, cropped_framebuffer], axis = -1)
+
+
+class Preprocess(ObservationWrapper):
+    def __init__(self, env, img_size = None):
+        ObservationWrapper.__init__(self,env)
+
+        if not img_size:
+            self.img_size = (env.observation_space.shape[0],
+                             env.observation_space.shape[1],
+                             1)
+                             
+        self.observation_space = Box(0.0, 1.0, self.img_size
+                                     , dtype=np.float32)
+
+    def observation(self, img):        
+        
+        # resize and normalize img
+        img = imresize(img, self.img_size)
+        img = img.mean(-1, keepdims=True)
+        img = img.astype('float32') / 255.
+        
+        return img
