7ea6726e45f98a343783619e6c1d6c7593b8495c
unknown <jnkami@gmail.com>
2019-11-04 20:31:58 -0300

    change to tf.keras

--- utils/models.py
+++ utils/models.py
@@ -74,32 +74,32 @@ def generic_model(state_ph,
                   activation = 'tanh'
                   ):
 
-    inputs = Input(tensor = state_ph)
+    inputs = tf.keras.layers.Input(tensor = state_ph)
     input_shape = state_ph.shape.as_list()
 
     if len(input_shape) > 2:   
-        x = Conv2D(filters=32, kernel_size=8, strides=4
+        x = tf.keras.layers.Conv2D(filters=32, kernel_size=8, strides=4
                                 ,activation=activation
                                 ,padding='valid'
                                 ,kernel_initializer=tf.variance_scaling_initializer(scale=2))(inputs) 
-        x = (Conv2D(filters=64, kernel_size=4, strides=2
+        x = tf.keras.layers.Conv2D(filters=64, kernel_size=4, strides=2
                                 ,activation=activation
                                 ,padding='valid'
-                                ,kernel_initializer=tf.variance_scaling_initializer(scale=2)))(x) 
-        x = Conv2D(filters=64, kernel_size=3, strides=1
+                                ,kernel_initializer=tf.variance_scaling_initializer(scale=2))(x) 
+        x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1
                                 ,activation=activation
                                 ,padding='valid'
                                 ,kernel_initializer=tf.variance_scaling_initializer(scale=2))(x)
-        x = Flatten()(x)
-        x = Dense(256, activation='relu'
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(256, activation='relu'
         ,kernel_initializer=tf.variance_scaling_initializer(scale=2))(x) 
-        x = Dense(action_n, activation = ouput_activation
+        x = tf.keras.layers.Dense(action_n, activation = ouput_activation
         ,kernel_initializer=tf.variance_scaling_initializer(scale=2))(x)
         return inputs, x
 
-    x = Dense(100, activation = activation)(inputs)
-    x = Dense(100, activation = activation)(x)
-    x = Dense(100, activation = activation)(x)
-    x = Dense(action_n, activation = ouput_activation)(x)
+    x = tf.keras.layers.Dense(100, activation = activation)(inputs)
+    x = tf.keras.layers.Dense(100, activation = activation)(x)
+    x = tf.keras.layers.Dense(100, activation = activation)(x)
+    x = tf.keras.layers.Dense(action_n, activation = ouput_activation)(x)
     return inputs, x
 
